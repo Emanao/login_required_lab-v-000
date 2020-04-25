24787d3eb65bcda264102558c95bf5c7bf0d95af
Eva Navarro <emanao@me.com>
2020-04-25 19:21:02 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,14 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    if params[:name].nil? || params[:name].strip.empty?
-      redirect_to login_path
-    else
-      session[:name]=params[:name]
+    if params.include?(:name) && !params[:name].strip.empty?
+      session[:name] = params[:name]
       redirect_to index_path
+    else
+      redirect_to login_path
     end
+  end
+  def show
   end
   def destroy
     session.clear
