22b3daaf9304665ba6c1ede811c09c740ee43867
Eva Navarro <emanao@me.com>
2020-04-25 20:46:28 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'login' => "sessions#new"
   post 'login' => "sessions#create"
-  get 'logout' => "sessions#destroy"
+  post 'logout' => "sessions#destroy"
 
   get 'index' => "secrets#index"
   get 'show' => "secrets#show"
